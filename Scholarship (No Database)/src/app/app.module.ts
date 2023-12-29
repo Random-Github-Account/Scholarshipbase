db3b14e5b042150d67822cd3e62fca90e63eeffb
Rein <132432835+Random-Github-Account@users.noreply.github.com>
2023-12-29 19:49:16 +0800

    Version 1

---
+++ Scholarship (No Database)/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { LoginModule } from './login/login.module';
+import { LayoutModule } from './layout/layout.module';
+import { RegisterModule } from './register/register.module';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ScreenComponent } from './components/screen/screen.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageModule } from './homepage/homepage.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule,
+    LayoutModule,
+    RegisterModule,
+    HomepageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Scholarship (No Database)/src/app/layout/layout-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+
+const routes : Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children:[
+      {
+        path: "",
+        redirectTo: "dashboard", pathMatch:"prefix",
+      },
+      {
+        path: "dashboard",
+        loadChildren: () => import('./dashboard/dashboard.module').then(mod => mod.DashboardModule),
+      },
+      {
+        path: "applications",
+        loadChildren: () => import('./applications/applications.module').then(mod => mod.ApplicationsModule),
+      },
+      {
+        path: "help",
+        loadChildren: () => import('./helpc/helpc.module').then(mod => mod.HelpcModule),
+      },
+      {
+        path: "notifications",
+        loadChildren: () => import('./notifications/notifications.module').then(mod => mod.NotificationsModule),
+      },
+      {
+        path: "messages",
+        loadChildren: () => import('./messages/messages.module').then(mod => mod.MessagesModule),
+      }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LayoutRoutingModule { }
