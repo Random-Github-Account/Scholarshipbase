4d91c6c3114dc65bf074396082bb3396255dcd07
Rein <132432835+Random-Github-Account@users.noreply.github.com>
2024-01-14 08:01:27 +0800

    Application example

--- Scholarship (Firebase)/src/app/layout/applications/applications.component.ts
+++ Scholarship (Firebase)/src/app/layout/applications/applications.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./applications.component.scss']
 })
 export class ApplicationsComponent {
+  applications: any[] = [
+    {
+      no: 1,
+      firstName: 'Nello',
+      lastName: 'De Guzman',
+      email: 'nellorethDG@gmail.com',
+      phone: 'xxxxxxxxxxx',
+      address: 'Makati, Manila',
+      scholarship: 'Athletic Scholarship'
+    },
+    {
+      no: 2,
+      firstName: 'Ian Kenneth',
+      lastName: 'Mata',
+      email: 'KianMata@gmail.com',
+      phone: 'xxxxxxxxxxx',
+      address: 'Quezon, Nueva Ecija',
+      scholarship: 'Academic Scholarship'
+    },
+    {
+      no: 3,
+      firstName: 'John Matthew',
+      lastName: 'Buenconsejo',
+      email: 'JMbuen@gmail.com',
+      phone: 'xxxxxxxxxxx',
+      address: 'Cabantuan, Nueva Ecija',
+      scholarship: 'Academic Scholarship'
+    },
+  ];
 
+  
+  deleteApplication(index: number): void {
+    if (index >= 0 && index < this.applications.length) {
+      this.applications.splice(index, 1);
+    }
+  }
 }
